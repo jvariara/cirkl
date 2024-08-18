0133824d82b2e412d123409b0512e94fbaeb4c30
jvariara <jvariara@gmail.com>
2024-08-18 12:03:55 -0400

    added menu bar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import React from "react";
 import SessionProvider from "./SessionProvider";
 import Navbar from "./Navbar";
+import MenuBar from "./MenuBar";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const session = await validateRequest();
@@ -14,7 +15,13 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
     <SessionProvider value={session}>
       <div className="flex min-h-screen flex-col">
         <Navbar />
-        <div className="max-w-7xl mx-auto p-5">{children}</div>
+        <div className="max-w-7xl mx-auto p-5 flex w-full grow gap-5">
+          {/* side bar for bigger screens */}
+          <MenuBar className="sticky top-[5.25rem] h-fit hidden sm:block flex-none space-y-3 rounded-2xl bg-card px-3 py-5 lg:px-5 shadow-sm xl:w-80" />
+          {children}
+        </div>
+        {/* Bottom bar for small screens */}
+        <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
       </div>
     </SessionProvider>
   );
