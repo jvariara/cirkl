0b5edca92adabb40e71ced36b6334a54f3d4d4cc
jvariara <jvariara@gmail.com>
2024-08-18 12:33:55 -0400

    added post creation

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,11 @@
+import PostEditor from "@/components/posts/editor/PostEditor";
+
 export default function Home() {
-  return <main>Front page</main>;
+  return (
+    <main className="w-full h-[200vh]">
+      <div className="w-full">
+        <PostEditor />
+      </div>
+    </main>
+  );
 }
