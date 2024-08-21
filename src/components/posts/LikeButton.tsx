a871b2432fe7c87e59c163b0d67eddb87f06e406
jvariara <jvariara@gmail.com>
2024-08-21 19:05:25 -0400

    added likes for posts

---
+++ src/components/posts/LikeButton.tsx
@@ -0,0 +1,81 @@
+import { LikeInfo } from "@/lib/types";
+import { useToast } from "../ui/use-toast";
+import {
+  QueryKey,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import kyInstance from "@/lib/ky";
+import { Heart } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface LikeButtonProps {
+  postId: string;
+  initialState: LikeInfo;
+}
+
+const LikeButton = ({ postId, initialState }: LikeButtonProps) => {
+  const { toast } = useToast();
+
+  const queryClient = useQueryClient();
+  const queryKey: QueryKey = ["like-info", postId];
+
+  const { data } = useQuery({
+    queryKey: ["like-info", postId],
+    queryFn: () =>
+      kyInstance.get(`/api/posts/${postId}/likes`).json<LikeInfo>(),
+    initialData: initialState,
+    staleTime: Infinity,
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: () =>
+      data.isLikedByUser
+        ? kyInstance.delete(`/api/posts/${postId}/likes`)
+        : kyInstance.post(`/api/posts/${postId}/likes`),
+    onMutate: async () => {
+      // optimistic update
+
+      // cancel the running queries
+      await queryClient.cancelQueries({ queryKey });
+
+      const previousState = queryClient.getQueryData<LikeInfo>(queryKey);
+
+      // modify the cache
+      queryClient.setQueryData<LikeInfo>(queryKey, () => ({
+        likes:
+          (previousState?.likes || 0) + (previousState?.isLikedByUser ? -1 : 1),
+        isLikedByUser: !previousState?.isLikedByUser,
+      }));
+
+      return { previousState };
+    },
+    onError: (error, variables, context) => {
+      // if there is an error, just set the data in the cache to the previous state
+      queryClient.setQueryData(queryKey, context?.previousState);
+
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Something went wrong. Please try again.",
+      });
+    },
+  });
+
+  return (
+    <button onClick={() => mutate()} className="flex items-center gap-2">
+      <Heart
+        className={cn(
+          "size-5",
+          data.isLikedByUser && "fill-red-500 text-red-500"
+        )}
+      />
+      <span className="text-sm font-medium tabular-nums">
+        {data.likes} <span className="hidden sm:inline">likes</span>
+      </span>
+    </button>
+  );
+};
+
+export default LikeButton;
