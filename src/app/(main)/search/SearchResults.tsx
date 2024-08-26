ff112cf47ac53a0955f9402406fade57731c275d
jvariara <jvariara@gmail.com>
2024-08-25 20:02:14 -0400

    added search feature

---
+++ src/app/(main)/search/SearchResults.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
+import Post from "@/components/posts/Post";
+import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
+import kyInstance from "@/lib/ky";
+import { PostsPage } from "@/lib/types";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
+
+interface SearchResultsProps {
+  query: string;
+}
+
+const SearchResults = ({ query }: SearchResultsProps) => {
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    error,
+    isPending,
+    isSuccess,
+  } = useInfiniteQuery({
+    queryKey: ["post-feed", "search", query],
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get("/api/search", {
+          searchParams: {
+            q: query,
+            ...(pageParam ? { cursor: pageParam } : {}),
+          },
+        })
+        .json<PostsPage>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+    gcTime: 0,
+  });
+
+  const posts = data?.pages.flatMap((page) => page.posts) || [];
+
+  // first page is loading
+  if (isPending) {
+    return <PostsLoadingSkeleton />;
+  }
+
+  // no posts
+  if (isSuccess && !posts.length && !hasNextPage) {
+    return (
+      <p className="text-center text-muted-foreground">
+        No posts found for &quot;{query}&quot;
+      </p>
+    );
+  }
+
+  if (error) {
+    return (
+      <p className="text-center text-destructive">
+        An error occured while loading posts.
+      </p>
+    );
+  }
+
+  return (
+    <InfiniteScrollContainer
+      className="space-y-5"
+      onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+    >
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+      {isFetchingNextPage && <Loader2 className="mx-auto my-3 animate-spin" />}
+    </InfiniteScrollContainer>
+  );
+};
+
+export default SearchResults;
